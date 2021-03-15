1e559407a0c6679cba48ec660abc1340ac8dbfb5
amin <57110542+aminzammou@users.noreply.github.com>
2021-03-15 19:44:15 +0100

    trainerService opgezet

---
+++ src/main/java/nl/hu/cisq1/lingo/trainer/application/TrainerService.java
@@ -0,0 +1,22 @@
+package nl.hu.cisq1.lingo.trainer.application;
+
+import lombok.AllArgsConstructor;
+import nl.hu.cisq1.lingo.trainer.data.GameEntity;
+import nl.hu.cisq1.lingo.trainer.data.SpringGameRepository;
+import nl.hu.cisq1.lingo.trainer.domain.Game;
+import nl.hu.cisq1.lingo.words.application.WordService;
+
+@AllArgsConstructor
+public class TrainerService {
+    private WordService wordService;
+    private SpringGameRepository gameRepository;
+
+    public Long startNewGame() {
+        Game game = new Game();
+        GameEntity gameEntity = new GameEntity(game);
+        this.gameRepository.save(gameEntity);
+
+        return gameEntity.getId();
+    }
+
+}
--- src/main/java/nl/hu/cisq1/lingo/trainer/data/SpringGameRepository.java
+++ src/main/java/nl/hu/cisq1/lingo/trainer/data/SpringGameRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface SpringGameRepository extends JpaRepository<Game, Long> {
+public interface SpringGameRepository extends JpaRepository<GameEntity, Long> {
 }
