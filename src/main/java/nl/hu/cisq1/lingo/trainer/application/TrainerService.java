91ead24acf537377f3f84b920712bcca2b77b7e2
amin <57110542+aminzammou@users.noreply.github.com>
2021-03-22 00:30:45 +0100

    startNewGame functie in TrainerService gemaakt

--- src/main/java/nl/hu/cisq1/lingo/trainer/application/TrainerService.java
+++ src/main/java/nl/hu/cisq1/lingo/trainer/application/TrainerService.java
@@ -3,6 +3,7 @@ package nl.hu.cisq1.lingo.trainer.application;
 import lombok.AllArgsConstructor;
 import nl.hu.cisq1.lingo.trainer.data.SpringGameRepository;
 import nl.hu.cisq1.lingo.trainer.domain.Game;
+import nl.hu.cisq1.lingo.trainer.domain.Progress;
 import nl.hu.cisq1.lingo.words.application.WordService;
 
 @AllArgsConstructor
@@ -10,12 +11,13 @@ public class TrainerService {
     private final WordService wordService;
     private final SpringGameRepository gameRepository;
 
-    public Long startNewGame() {
+    public Progress startNewGame() {
         Game game = new Game();
-//        GameEntity gameEntity = new GameEntity(game);
-        this.gameRepository.save(game);
 
-        return game.getId();
+        this.gameRepository.save(game);
+        String wordToGuess = this.wordService.provideRandomWord(game.getWordLength());
+        return game.startNewGame(wordToGuess);
     }
+
 
 }
