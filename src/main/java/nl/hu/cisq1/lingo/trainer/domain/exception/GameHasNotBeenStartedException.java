a6e659fe39cf0683eda0873c764da58be0174ce7
amin <57110542+aminzammou@users.noreply.github.com>
2021-04-11 18:39:29 +0200

    code coverage verbeterd

--- src/main/java/nl/hu/cisq1/lingo/trainer/domain/exception/GameHasNotBeenStartedException.java
+++
@@ -1,7 +0,0 @@
-package nl.hu.cisq1.lingo.trainer.domain.exception;
-
-public class GameHasNotBeenStartedException extends RuntimeException {
-    public GameHasNotBeenStartedException() {
-        super("You can not guess yet, the game has not been started!");
-    }
-}
